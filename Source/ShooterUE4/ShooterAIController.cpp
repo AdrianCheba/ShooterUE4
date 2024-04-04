5510ccf5316080e66268904be90201437f4d096d
Adrian Cheba <adiche@wp.pl>
2024-04-04 22:26:39 +0200

    Added simply behavior tree tasks

--- Source/ShooterUE4/ShooterAIController.cpp
+++ Source/ShooterUE4/ShooterAIController.cpp
@@ -3,18 +3,29 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
-
-void AShooterAIController::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	
-	MoveToActor(PlayerPawn, 200);
-}
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AShooterAIController::BeginPlay()
 {
 	Super::BeginPlay();
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	SetFocus(PlayerPawn);
+	if (AIBehavior)
+	{
+		RunBehaviorTree(AIBehavior);
+		
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+	}
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+	}
+	else
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
 }
